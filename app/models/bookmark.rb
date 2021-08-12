5bca98fb9441163e2d4e29c7cc976ba43a0d5aad
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-12 11:49:03 +0200

    Created models Movie, List and Bookmark with validations and associations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates_associated :movie
+  validates_associated :list
+  validates_uniqueness_of :movie_id, :scope => [:list_id]
+  validates :comment, length: { minimum: 6 }
+end
